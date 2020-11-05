aa49fb33bd8e34f12bf6769aa83004cb739bb15b
Lynext <lynext12@gmail.com>
2020-11-05 19:16:52 +0300

    Lynext
    BPM Difference is calculated better

--- SongSimilarityClass.cs
+++ SongSimilarityClass.cs
@@ -12,9 +12,35 @@ namespace Jagger
         public Song similarSong;
         public string Name { get { return similarSong.Name; } set { } }
         public int BPM { get { return similarSong.BPM; } set { } }
+
+        public string BPMString { get {
+                int mn = Math.Min(similarSong.BPM, baseSong.BPM);
+                int mx = Math.Max(similarSong.BPM, baseSong.BPM);
+                int firstDiff = Math.Abs(mx - mn);
+                int secondDiff = Math.Abs(mx / 2 - mn);
+                // FOR DEBUG
+                int thirdDiff = Math.Abs(mn * 2 - mx);
+                if (firstDiff <= secondDiff)
+                {
+                    return BPM.ToString();
+                }
+                if (secondDiff <= thirdDiff)
+                {
+                    if (mn == similarSong.BPM)
+                    {
+                        return BPM.ToString() + " (B " + (mx / 2).ToString() + ")";
+                    }
+                    return BPM.ToString() + " (" + (mx / 2).ToString() + ")";
+                }
+                return "!!!!!";
+            } set { } }
         public string Key { get { return similarSong.Key; } set { } }
         public string Artists { get { return similarSong.Artists; } set { } }
-        public int BPMDifference { get { return Math.Abs(similarSong.BPM - baseSong.BPM); } set { } }
+        public int BPMDifference { get {
+                int mn = Math.Min(similarSong.BPM, baseSong.BPM);
+                int mx = Math.Max(similarSong.BPM, baseSong.BPM);
+                return Math.Min(Math.Abs(mx - mn), Math.Abs(mx / 2 - mn));
+            } set { } }
         public int keyDifference { get { return Helper.getKeyDiff(baseSong.Key, similarSong.Key); } set { } }
         public int similarityPercent
         {
